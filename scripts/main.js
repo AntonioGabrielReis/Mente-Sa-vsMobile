8401b2cc07aa0b358693c91e6d2a1a6712b10ace
AntonioGabrielReis <antoniobielreis@gmail.com>
2024-05-31 11:18:45 -0300

    sombra

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,43 @@
 function toggleMenu() {
     const sidebarMenu = document.getElementById('sidebar-menu');
     const menuButton = document.getElementById('menu-button');
-    const isExpanded = menuButton.getAttribute('aria-expanded') === 'true';
+    const sidebarAgendamento = document.getElementById('sidebar-agendamento');
+    const agendamentoButton = document.getElementById('agendamento-button');
+    const isMenuExpanded = menuButton.getAttribute('aria-expanded') === 'true';
+    const isAgendamentoExpanded = agendamentoButton.getAttribute('aria-expanded') === 'true';
 
-    if (isExpanded) {
+    if (isMenuExpanded) {
         sidebarMenu.style.left = '-250px';
         menuButton.setAttribute('aria-expanded', 'false');
     } else {
         sidebarMenu.style.left = '0';
         menuButton.setAttribute('aria-expanded', 'true');
+        // Close the agendamento menu if it is open
+        if (isAgendamentoExpanded) {
+            sidebarAgendamento.style.left = '-250px';
+            agendamentoButton.setAttribute('aria-expanded', 'false');
+        }
     }
 }
 
 function toggleAgendamento() {
     const sidebarAgendamento = document.getElementById('sidebar-agendamento');
     const agendamentoButton = document.getElementById('agendamento-button');
-    const isExpanded = agendamentoButton.getAttribute('aria-expanded') === 'true';
+    const sidebarMenu = document.getElementById('sidebar-menu');
+    const menuButton = document.getElementById('menu-button');
+    const isAgendamentoExpanded = agendamentoButton.getAttribute('aria-expanded') === 'true';
+    const isMenuExpanded = menuButton.getAttribute('aria-expanded') === 'true';
 
-    if (isExpanded) {
+    if (isAgendamentoExpanded) {
         sidebarAgendamento.style.left = '-250px';
         agendamentoButton.setAttribute('aria-expanded', 'false');
     } else {
         sidebarAgendamento.style.left = '0';
         agendamentoButton.setAttribute('aria-expanded', 'true');
+        // Close the menu if it is open
+        if (isMenuExpanded) {
+            sidebarMenu.style.left = '-250px';
+            menuButton.setAttribute('aria-expanded', 'false');
+        }
     }
 }
